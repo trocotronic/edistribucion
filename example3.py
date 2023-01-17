ef5fcd7c5ce98387687766ac482f24ea885cb745
Trocotronic <trocotronic@redyc.com>
2023-01-17 14:11:17 +0100

    Added support for multiple identities management.
    Now, methods that manage CUPS accept vis parameter to specify the identity of the owner. If not specified, it uses the default identity. It includes an example.
    
    Signed-off-by: Trocotronic <trocotronic@redyc.com>

---
+++ example3.py
@@ -0,0 +1,29 @@
+"""
+Created on Tue Jan 17 11:51:36 2022
+
+@author: trocotronic
+"""
+
+'''
+This example shows how to manage multiple identities and authorizations. It allows, for instance, to get info from other points without requiring credentials.
+Note: your user must be authorized by the owner of supply.
+'''
+
+from EdistribucionAPI import Edistribucion
+
+edis = Edistribucion()
+edis.login()
+identities = edis.get_identities()
+## We use the first identity
+vis = identities[0]['value']
+cups_list = edis.get_list_cups(vis=vis)
+print(cups_list)
+cups = edis.get_cups(vis=vis)
+print(cups)
+cups = edis.get_all_cups(vis=vis)
+print(cups)
+## We use the latest CUPS from the selected identity vis
+cups_detail = edis.get_cups_detail(cups=cups_list[-1]['CUPS_Id'], vis=vis)
+print(cups_detail)
+cups_info = edis.get_cups_info(cups=cups_list[-1]['CUPS_Id'], vis=vis)
+print(cups_info)
