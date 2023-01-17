94431a5f0a28eac99d8dcb862504e7b349651dd6
Trocotronic <trocotronic@redyc.com>
2023-01-17 14:13:03 +0100

    get_list_cycles(), get_meas() and get_meas_interval() now accept the cups[Id] parameter directly (the counter id).
    The example is updated.
    
    Signed-off-by: Trocotronic <trocotronic@redyc.com>

--- example2.py
+++ example2.py
@@ -4,15 +4,19 @@ Created on Wed May 20 11:51:36 2020
 @author: trocotronic
 """
 
+'''
+This example show how to call cycle and measurements panel.
+'''
+
 from EdistribucionAPI import Edistribucion
 
 edis = Edistribucion()
 edis.login()
-r = edis.get_cups()
 cups = edis.get_list_cups()[-1]
-print('Cups: ',cups['Id'])
+print('Cups: ',cups['CUPS_Id'])
 info = edis.get_cups_info(cups['CUPS_Id'])
 print(info)
-cycles = edis.get_list_cycles(cups)
-meas = edis.get_meas(cups, cycles[0]) # Contains all measured points per hour for the first cycle
-meas = edis.get_meas_interval(cups, '2022-12-01', '2022-12-31')
+cycles = edis.get_list_cycles(cups['Id'])
+meas = edis.get_meas(cups['Id'], cycles[0]) # Contains all measured points per hour for the first cycle
+meas = edis.get_meas_interval(cups['Id'], '2022-12-01', '2022-12-31')
+
